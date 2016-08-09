2160f806d3887411d3d34d33789b9d86d06530cb
Sander Lepik <sander@lepik.eu>
2016-08-09 22:43:35 +0300

    Check if path to wallpaper is given

--- screensaver/xscreen-lock.sh
+++ screensaver/xscreen-lock.sh
@@ -10,14 +10,13 @@ touch /tmp/xscreen-lock
 # Use xset s $time to control the timeout when this will run.
 xset s 300
 
+cmd="i3lock"
 # Check if the 1st argument is file
-if [ -f $1 ]; then
+if [ $# -eq 1 ] && [ -f $1 ]; then
 	mime=$(file --mime-type $1 | cut -d \  -f2)
 	# Check if it's a PNG file
 	if [ $(echo $mime|grep -i png|wc -l) -eq 1 ]; then
 		cmd="i3lock -i $1"
-	else
-		cmd="i3lock"
 	fi
 fi
 
