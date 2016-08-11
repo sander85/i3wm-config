09cf574280e49b0071747d863ee0cad4b042f24f
Sander Lepik <sander@lepik.eu>
2016-08-11 15:30:54 +0300

    Move local conf in its own directory

--- bin/merge-conf.sh
+++ bin/merge-conf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat ~/.i3wm-config/i3-base.conf ~/.i3wm-config/i3-$(hostname).conf > ~/.config/i3/config
+cat ~/.i3wm-config/i3-base.conf ~/.i3wm-config/local/i3-$(hostname).conf > ~/.config/i3/config
